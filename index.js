bc60ec13a4ebec280e70a65f4a76d34b2af71ca5
harisabdullah <hariskhan1435@gmail.com>
2023-06-14 11:19:42 +0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const promptSense = require('./prompt-sense');
-
-const prompt = 'Enter a city name: ';
-const suggestions = ['New York', 'New Delhi', 'New Jersey', 'New Mexico', 'New Orleans', 'New Hampshire', 'New Haven', 'Newark', 'Newport', 'New Britain'];;
-const numberOfSuggestions = 10;
-
-promptSense(prompt, suggestions, numberOfSuggestions).then((result) => {
-  console.log(`You entered: ${result}`);
-  process.exit();
-});
+export * from './prompt-sense';
